1d4bf9f9b24e207356e8e206f2ab44cc3f3d0eae
amaster507 <themaster5.07@gmail.com>
2023-01-31 22:58:48 +0000

    initial commit of gofer Engine

---
+++ jest.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from 'jest'
+
+const config: Config = {
+  roots: ['<rootDir>'],
+  testMatch: [
+    '**/__tests__/**/*.+(ts|tsx|js)',
+    '**/?(*.)+(spec|test).+(ts|tsx|js)',
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      './config/fileTransformer.js',
+  },
+  globals: {
+    transform: {
+      '^.+\\.(ts|tsx)$': ['ts-jest', 'tsconfig.json'],
+    },
+  },
+  fakeTimers: {
+    enableGlobally: true,
+    now: new Date('2023-01-11T00:15:27.216Z').getTime(),
+  },
+}
+
+export default config
---
+++ src/channels/SampleB.ts
@@ -0,0 +1,13 @@
+import { ServerConfig } from '../types'
+
+const SampleB: ServerConfig = {
+  name: 'SampleB',
+  organization: '$MSH-5',
+  host: '192.168.15.201',
+  port: 9001,
+  store: {
+    file: {},
+  },
+}
+
+export default SampleB
