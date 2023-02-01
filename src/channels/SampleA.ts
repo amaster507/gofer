a63a821ed0f5e1a2ca3900d28b791eca32890c1c
amaster507 <themaster5.07@gmail.com>
2023-02-01 21:28:08 +0000

    refactor channel config types and samples

--- src/channels/SampleA.ts
+++ src/channels/SampleA.ts
@@ -1,19 +1,29 @@
-import { ServerConfig } from '../types'
+import { ChannelConfig } from '../types'
 
-const SampleA: ServerConfig = {
+const SampleA: ChannelConfig = {
   name: 'Sample A',
-  organization: 'My Organization',
-  host: '192.168.15.201',
-  port: 9002,
-  store: {
-    surreal: {
-      table: '$MSH-9.1',
-      namespace: 'MyNamespace',
-      database: '$MSH-3',
-      uri: 'http://10.3.54.148:8000/rpc',
-      id: '$MSH-10',
+  source: {
+    tcp: {
+      host: '192.168.15.201',
+      port: 9002,
     },
   },
+  ingestion: [
+    {
+      ack: {
+        organization: 'My Organization',
+      },
+    },
+    {
+      surreal: {
+        table: '$MSH-9.1',
+        namespace: 'MyNamespace',
+        database: '$MSH-3',
+        uri: 'http://10.3.54.148:8000/rpc',
+        id: '$MSH-10',
+      },
+    },
+  ],
 }
 
 export default SampleA
--- src/types.ts
+++ src/types.ts
@@ -10,19 +10,17 @@ type RequireOnlyOne<T, Keys extends keyof T = keyof T> = Pick<
       Partial<Record<Exclude<Keys, K>, undefined>>
   }[Keys]
 
-export interface ServerConfig {
-  // The receiving server host name/ip
+type RequireAtLeastOne<T, Keys extends keyof T = keyof T> = Pick<
+  T,
+  Exclude<keyof T, Keys>
+> &
+  {
+    [K in Keys]-?: Required<Pick<T, K>> & Partial<Pick<T, Exclude<Keys, K>>>
+  }[Keys]
+
+interface ITcpConfig {
   host: string
-  // The receiving server port
   port: number
-  // Value to use in ACK MSH.3
-  name: string
-  // Value to use in ACK MSH.4
-  organization: string
-  // A Store configuration to save persistent messages
-  store?: StoreConfig
-  // TODO: add support for outbound connections including filters, transformers, and destinations
-  route?: ServerRoute[]
 }
 
 export type StoreConfig = RequireOnlyOne<{
@@ -30,27 +28,88 @@ export type StoreConfig = RequireOnlyOne<{
   surreal: StoreOptions['surreal']
 }>
 
-// FIXME: Support different type of routes not just TCP
-interface ServerRoute {
-  // The destination server host name/ip
-  host: string
-  // The destination server port
-  port: number
-  // Filters, Transformers, and Stores to apply to the message
-  // These will get applied sequentially.
-  flows?: (FilterFlow | TransformFlow | StoreConfig)[]
-  // Whether or not to wait for the previous Route to complete before starting this one.
-  // Ignored on first Route
-  wait?: boolean
-  // Whether to use original message or final message from previous Route
-  // Ignored on first Route
-  // Defaults to true
-  useOriginal?: boolean
-  // Whether to queue messages to the destination or just drop them
-  // TODO: add support for queueing. Need more options for this, TBD
-  queue?: boolean
+interface Queue {
+  interval?: number
+  limit?: number
+  rotate?: boolean
+  storage?: StoreConfig
+}
+
+export type TcpConfig<T extends 'I' | 'O' = 'I'> = T extends 'I'
+  ? ITcpConfig
+  : ITcpConfig & {
+      queue?: boolean | number | Queue
+      responseTimeout?: number | false
+    }
+
+interface IFileConfig {
+  directory: string
+  ftp?: string
+  sftp?: string
+  filenamePattern?: string
+  includeAllSubDirs?: boolean
+  ignoreDotFiles?: boolean
+  username?: boolean
+  password?: boolean
+  timeout?: number
+  deleteAfterRead?: boolean
+  moveAfterRead?: {
+    directory: string
+    filename: string
+  }
+  moveAfterError?: {
+    directory: string
+    filename: string
+  }
+  checkFileAge?: number
+  limitSize?: RequireAtLeastOne<{
+    min?: number
+    max?: number
+  }>
+}
+
+export type FileConfig = RequireOnlyOne<
+  IFileConfig,
+  'ftp' | 'sftp' | 'directory'
+>
+
+export type Connection<T extends 'I' | 'O'> = RequireOnlyOne<{
+  tcp?: TcpConfig<T> // Listens/Sends on/to a TCP host/port
+  // file?: FileConfig // Read/Write Files
+  db?: StoreConfig // Queries/Mutates from/to Store
+}>
+
+export type AckConfig = {
+  // Value to use in ACK MSH.3
+  application?: string // defaults to "gofor Engine"
+  // Value to use in ACK MSH.4
+  organization?: string // defaults to empty string ""
+  // A Store configuration to save persistent messages
+  msg?: (ack: Msg, msg: Msg) => Msg // returns the ack message to send
+}
+
+interface Tag {
+  name: string
+  color?: string // a valid hexidecimal color string or valid CSS color name
 }
 
 type FilterFlow = (msg: Msg) => boolean
 
 type TransformFlow = (msg: Msg) => Msg
+
+type IngestionFlow =
+  | { ack: AckConfig }
+  | FilterFlow
+  | TransformFlow
+  | StoreConfig
+
+type RouteFlow = IngestionFlow | Connection<'O'>
+
+export interface ChannelConfig {
+  id?: string | number // a unique id for this channel. If not provided will use UUID to generate. if not defined it may not be the same between deployments/reboots
+  name: string // a name, preferrably unique, to identify this channel later on
+  tags?: Tag[] // Tags to help organize/identify channels
+  source: Connection<'I'>
+  ingestion: IngestionFlow[]
+  routes?: RouteFlow[][]
+}
