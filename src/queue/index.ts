91ca9d4591ad854b14dc92c04de1fc039f4d9728
Anthony Master <themaster5.07@gmail.com>
2023-03-14 08:31:36 -0500

    make queue function async

--- src/queue/index.ts
+++ src/queue/index.ts
@@ -10,22 +10,27 @@ export const addToQueue = <T = Msg>(
   task: (data: T) => Promise<boolean>
 ) => {
   if (queues.hasOwnProperty(id)) return (queues[id] as BetterQueue<T>).push
-  const queue = new BetterQueue<T>((data, cb) => {
-    return task(data).then((success) => {
-      cb(null, success)
-    }).catch((e) => cb(e))
-  }, {
-    retryDelay: config.interval ?? 10 * 1000,
-    filo: config.filo ?? false,
-    maxRetries: config.retries ?? Infinity,
-    id: config.id,
-    filter: config.filterQueue,
-    precondition: config.precondition,
-    preconditionRetryTimeout: config.preconditionRetryTimeout,
-    concurrent: config.concurrent ?? 1,
-    maxTimeout: config.maxTimeout ?? 10 * 1000,
-    afterProcessDelay: config.afterProcessDelay ?? 1,
-  })
+  const queue = new BetterQueue<T>(
+    async (data, cb) => {
+      return task(data)
+        .then((success) => {
+          cb(null, success)
+        })
+        .catch((e) => cb(e))
+    },
+    {
+      retryDelay: config.interval ?? 10 * 1000,
+      filo: config.filo ?? false,
+      maxRetries: config.retries ?? Infinity,
+      id: config.id,
+      filter: config.filterQueue,
+      precondition: config.precondition,
+      preconditionRetryTimeout: config.preconditionRetryTimeout,
+      concurrent: config.concurrent ?? 1,
+      maxTimeout: config.maxTimeout ?? 10 * 1000,
+      afterProcessDelay: config.afterProcessDelay ?? 1,
+    }
+  )
   const events = config.onEvents ?? []
   events.forEach(([event, listener]) => {
     queue.on(event, listener)
